3ccdccf99c62e30f53874e6d458ede37bf780d6c
GabrielGrinchy <153121778+GabrielGrinchy@users.noreply.github.com>
2026-05-27 10:16:19 +0200

    Delete tintenstrahl-app-FIX-github-upload directory

--- tintenstrahl-app-FIX-github-upload/src/utils/geometry.js
+++
@@ -1,7 +0,0 @@
-export function getTopicPosition(angle, radius) {
-  const radians = (angle * Math.PI) / 180;
-  return {
-    x: Math.cos(radians) * radius,
-    y: Math.sin(radians) * radius
-  };
-}
